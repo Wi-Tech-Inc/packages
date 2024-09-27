967e35abf95ea02cf18c862fe9b2e5ad49cf40e0
Alexander Troshkov <20989940+aednlaxer@users.noreply.github.com>
2024-09-27 09:44:42 +0300

    Move advanced markers page to main example

--- packages/google_maps_flutter/google_maps_flutter_android/example/lib/main.dart
+++ packages/google_maps_flutter/google_maps_flutter_android/example/lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
-import 'advanced_markers.dart';
 import 'animate_camera.dart';
 import 'clustering.dart';
 import 'ground_overlay.dart';
@@ -37,7 +36,6 @@ final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
   const MoveCameraPage(),
   const PlaceMarkerPage(),
   const MarkerIconsPage(),
-  const AdvancedMarkersPage(),
   const ScrollingMapPage(),
   const PlacePolylinePage(),
   const PlacePolygonPage(),
